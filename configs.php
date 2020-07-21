a10d2755852f11e0c7e46232c843f0ee20462600
nscoincommunity <64058715+nscoincommunity@users.noreply.github.com>
2020-07-21 16:03:06 +0700

    Create configs.php

---
+++ configs.php
@@ -0,0 +1,14 @@
+
+<?php
+
+return [
+    'coinname' => 'Dogecoin',
+    'rpchost' => '139.180.211.157',
+    'rpcport' => 22555,
+    'rpcusername' => 'dogecoin',
+    'rpcpassword' => 'dragonexchange',
+    'blocksperpage' => 50,
+
+];
+
+?>
